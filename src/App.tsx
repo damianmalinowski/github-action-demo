fb1ac63098e719da2710c081f5363f6a7468defd
Damian Malinowski <damian.malinowski@axbit.com>
2020-04-07 17:12:48 +0200

    time

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Github Action Demo after 17:09</p>
+        <p>Github Action Demo after 17:12</p>
       </header>
     </div>
   );
